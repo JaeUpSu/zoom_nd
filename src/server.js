208b3ab3d821a680e667badd49d9187c77e62665
khs <hyeongs2323@gmail.com>
2023-10-22 18:07:04 +0900

    handler add

--- src/server.js
+++ src/server.js
@@ -2,6 +2,5 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
-
-app.listen(3000);
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListen);
